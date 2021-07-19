df399d10647848341b9bb74b9e62a5c0edbf9dcb
Sebastien BONELLI <sebastien.bonelli@agap2-it.fr>
2021-07-19 10:09:44 +0200

    .

---
+++ CSharpLearn/CSharpLearn/01_HeritageInterfacesProtected/HeritageDemo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLearn._01_HeritageInterfacesProtected
+{
+    public class HeritageDemo
+    {
+        public void Execute()
+        {
+
+        }
+    }
+}
--- CSharpLearn/CSharpLearn/Program.cs
+++ CSharpLearn/CSharpLearn/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSharpLearn._01_HeritageInterfacesProtected;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,9 @@ namespace CSharpLearn
     {
         static void Main(string[] args)
         {
+            //Heritage
+            var heritageDemo = new HeritageDemo();
+            heritageDemo.Execute();
         }
     }
 }
